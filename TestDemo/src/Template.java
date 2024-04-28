ddb638b26417650c54a61529964ab5f98da858c0
Joshkriti <Komalkanji13@gmail.com>
2024-04-28 20:18:48 +0100

    my first push

---
+++ TestDemo/src/Template.java
@@ -0,0 +1,9 @@
+public class Template {
+
+    String name = "Komall";
+
+    public static void main(String[] args) {
+        Template t = new Template();
+        System.out.println(t.name);
+    }
+}
