4dcc5b823f196f4a3dace914ea3411d9abe41c36
Joshkriti <Komalkanji13@gmail.com>
2024-04-29 17:22:19 +0100

    First push

---
+++ TestDemo/src/week9homework/Question_11.java
@@ -0,0 +1,19 @@
+package week9homework;
+
+public class Question_11 {
+
+    /*
+    11. Declare global variables of your name, Add all your group names in list, using
+for each loop iterate and print ONLY your name.
+     */
+
+    public static void main(String[] args) {
+        add("Komal");
+
+    }
+    public static void add(String name){
+        System.out.println(name);
+
+    }
+
+}
---
+++ TestDemo/src/week9homework/Question_7.java
@@ -0,0 +1,30 @@
+package week9homework;
+
+import java.util.HashMap;
+
+public class Question_7 {
+    /*
+    7. Create a HashMap object called people that will store String keys and Integer
+values: And use for each loop to iterate the value from Map.
+     */
+
+    public static void main(String[] args) {
+        HashMap<Integer, String> people = new HashMap<>();
+        people.put(21, "Prisha");
+        people.put(24, "Krisha");
+        people.put(29, "Mishta");
+
+        System.out.println(people);
+
+        for (Integer a : people.keySet()){
+            System.out.println(a);
+        }
+
+        for (String b : people.values() ){
+            System.out.println(b);
+        }
+    }
+
+
+
+}
